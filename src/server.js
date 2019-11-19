5c8a5d78cb68e6057a3a0846877577460e5b1976
Cara Steffen <cara@treadstonedesign.com>
2019-11-18 19:45:14 -0700

    preparing to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/api/*", (req, res) => {
   res.json({ ok: true });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  //console.log(`Server listening at http://localhost:${PORT}`);
 });
