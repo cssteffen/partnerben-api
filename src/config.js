65fa31bff4248e2439ba60b0d3d0e61a791c239b
Cara Steffen <cara@treadstonedesign.com>
2019-12-02 15:33:02 -0700

    working on retrieving single paycheck for dashboard

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/partnerben",
-  TEST_DB_URL: process.env.DB_URL
+  TEST_DB_URL: process.env.TEST_DB_URL
   //API_BASE_URL:
   //process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
 };
